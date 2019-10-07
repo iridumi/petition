a887a20616bbe4727047a6af56f614b105b3158f
Irina Dumitriu <irina.dumitriu@gmail.com>
2019-10-07 21:33:38 +0200

    canvas added

---
+++ public/sign.js
@@ -0,0 +1,46 @@
+//(function signPetition() {
+let canvas = document.getElementById("canvas");
+let submit = document.getElementById("submit");
+let hidden = document.querySelector("hidden");
+let c = canvas.getContext("2d");
+let signing = false;
+let x, y;
+
+canvas.addEventListener("mousedown", function(e) {
+    x = e.pageX - canvas.offsetLeft;
+    y = e.pageY - canvas.offsetTop;
+    signing = true;
+});
+
+canvas.addEventListener("mousemove", function(e) {
+    if (signing === true) {
+        draw(c, x, y, e.pageX - canvas.offsetLeft, e.pageY - canvas.offsetTop);
+        x = e.pageX - canvas.offsetLeft;
+        y = e.pageY - canvas.offsetTop;
+    }
+});
+
+document.addEventListener("mouseup", function(e) {
+    if (signing === true) {
+        draw(c, x, y, e.pageX - canvas.offsetLeft, e.pageY - canvas.offsetTop);
+        x = 0;
+        y = 0;
+        signing = false;
+    }
+});
+
+function draw(c, xInit, yInit, xFin, yFin) {
+    c.beginPath();
+    c.strokeStyle = "black";
+    c.lineWidth = 2;
+    c.moveTo(xInit, yInit);
+    c.lineTo(xFin, yFin);
+    c.stroke();
+    c.closePath();
+}
+
+submit.addEventListener("click", () => {
+    hidden.value = canvas.toDataURL();
+    //console.log(hidden.value);
+});
+//})();
